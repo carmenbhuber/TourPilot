d9975e52ea33ee18930e08d348b2ff1c21cc1163
carmenbhuber <49204833+carmenbhuber@users.noreply.github.com>
2026-06-16 13:25:27 +0200

    Use original EG map asset in map modal

---
+++ tourpilot-map-patch.js
@@ -0,0 +1,5 @@
+if (typeof MAPS !== 'undefined' && MAPS.EG) {
+  MAPS.EG.title = 'Karte EG';
+  MAPS.EG.subtitle = 'Originalplan EG mit grüner Wegführung – Test auf Handy-Lesbarkeit';
+  MAPS.EG.html = '<img class="mapImage" src="assets/eg-map.svg" alt="EG Plan mit Wegführung">';
+}
